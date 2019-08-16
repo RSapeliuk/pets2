f046f192f6752bda57d51d4d3a005f4f4181d9cb
rsapeliuk <rsapeliuk@gmail.com>
2019-08-16 16:41:01 +0300

    Initial commit

---
+++ src/main/java/com/petssocial/pets2/Pets2Application.java
@@ -0,0 +1,13 @@
+package com.petssocial.pets2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pets2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pets2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/petssocial/pets2/Pets2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.petssocial.pets2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Pets2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
